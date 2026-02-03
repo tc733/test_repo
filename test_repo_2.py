599e01c754e2893ca895cbde6a271a817401a400
tc733 <tc733@cam.ac.uk>
2026-02-03 20:25:38 +0000

    another test

--- test_repo_2.py
+++ test_repo_2.py
@@ -2,10 +2,6 @@ print("hiii world!!!")
 
 print("I have added this directly from github.")
 
-<<<<<<< HEAD
-#hello hi hello
-print("will this get deleted?")
-=======
 
 print("Will there be a conflict between these two?")
->>>>>>> 62077223481c97e6048cf529010d151420add95e
+
